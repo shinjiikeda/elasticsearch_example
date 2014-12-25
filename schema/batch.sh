8efbee948eae21bf81b9917001e52c8084551a85
Shinji Ikeda <ike_s@Shinji-no-MacBook-Pro.local>
2014-12-25 20:06:56 +0900

    Revert "update"
    This reverts commit 3a5a38ca6345411eb6433b1cc6740f4fc52d6748.

--- schema/batch.sh
+++ schema/batch.sh
@@ -1,10 +1,5 @@
-HOST="192.168.255.1:19200"
-
-echo mapping
-curl -XPUT "http://$HOST/news/" -d @mapping.json
-
+curl -XPUT 'http://localhost:9200/search/' -d @mapping.json
 echo
-echo schame
-curl -XPUT "http://$HOST/news/_mapping/20141222" -d @schema_news_20141222.json
+curl -XPUT 'http://localhost:9200/search/_mapping/test' -d @schema_test.json
 echo
 
