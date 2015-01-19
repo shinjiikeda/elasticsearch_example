9952f240fc0d3758b225ce04528ebc86c79c9e9d
Shinji Ikeda <ike_s@Shinji-no-MacBook-Pro.local>
2015-01-19 23:31:10 +0900

    update

--- schema/batch.sh
+++ schema/batch.sh
@@ -1,5 +1,7 @@
-curl -XPUT 'http://localhost:9200/search/' -d @mapping.json
+HOST=localhost
+
+curl -XPUT "http://$HOST:9200/search/" -d @mapping.json
 echo
-curl -XPUT 'http://localhost:9200/search/_mapping/test' -d @schema_test.json
+curl -XPUT "http://$HOST:9200/search/_mapping/test" -d @schema_test.json
 echo
 
